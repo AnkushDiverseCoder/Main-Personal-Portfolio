b521102ce833dc5f3c8609755f5264be00cf23da
AnkushDiverseCoder <thakurankushsingh1902@gmail.com>
2023-05-20 18:01:22 +0530

    v1.2.0

--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -1,4 +1,4 @@
-import Logo from "../assets/Logo.png"
+import Logo from "../assets/logo.png"
 import ProfileImg from "../assets/ProfileImg.png"
 import Project1 from "../assets/Project1.jpg"
 import youtubeClone from "../assets/YoutubeClone.jpg"
